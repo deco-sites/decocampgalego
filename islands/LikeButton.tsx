5dafd2ff80258f0cbc21a37e8572f113f435f5af
Gabriel Almeida <gabriel@agenciaeplus.com.br>
2024-04-13 15:57:59 -0300

    correcting toastify

--- islands/LikeButton.tsx
+++ islands/LikeButton.tsx
@@ -3,7 +3,7 @@ import Icon from "$components/ui/Icon.tsx";
 import { invoke } from "deco-sites/decocampgalego/runtime.ts";
 import { total } from "$sdk/useTotalLikes.ts";
 import { useEffect } from "preact/hooks";
-import { ToastContainer, toast, Bounce } from 'react-toastify';
+import { Bounce, toast, ToastContainer } from "react-toastify";
 
 export interface LikeButtonIslandProps {
   productID: string;
@@ -12,18 +12,8 @@ export interface LikeButtonIslandProps {
 function LikeButton({ productID }: LikeButtonIslandProps) {
   const selected = useSignal(false);
   const quantity = useSignal(0);
-  const notify = () => toast('👍 Curtiu meeeeeu!', {
-    position: "top-right",
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: false,
-    draggable: true,
-    progress: undefined,
-    theme: "light",
-    });;
+    
   const Toast = ToastContainer as any;
-
 
   useEffect(() => {
     const updateTotals = async () => {
@@ -54,38 +44,37 @@ function LikeButton({ productID }: LikeButtonIslandProps) {
     const totalLikesProduct = await invoke["deco-sites/decocampgalego"].loaders
       .totalLikesProductLoader({ productID });
     quantity.value = totalLikesProduct.product;
-    notify();
-    
+    toast.success("👍 Curtiu meeeeeu!", {
+      position: "top-right",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: Bounce,
+    });
   };
 
   return (
     <>
-    <button
-      class="absolute left-4 sm:left-auto sm:right-4 top-4 flex items-center justify-center gap-1 p-1 sm:p-2 rounded bg-neutral sm:bg-white min-w-14"
-      onClick={(e) => handleToggleLike(e)}
-    >
-      {!selected.value
-        ? <Icon id="MoodSmile" width={24} height={24} />
-        : <Icon id="MoodCheck" width={24} height={24} />}
-      <span
-        class={`min-w-4 text-center text-xs font-thin ${
-          !selected.value ? "text-gray-500" : "text-secondary"
-        }`}
+      <button
+        class="absolute left-4 sm:left-auto sm:right-4 top-4 flex items-center justify-center gap-1 p-1 sm:p-2 rounded bg-neutral sm:bg-white min-w-14"
+        onClick={(e) => handleToggleLike(e)}
       >
-        {quantity.value}
-      </span>
-    </button>
-    <Toast position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-        />
+        {!selected.value
+          ? <Icon id="MoodSmile" width={24} height={24} />
+          : <Icon id="MoodCheck" width={24} height={24} />}
+        <span
+          class={`min-w-4 text-center text-xs font-thin ${
+            !selected.value ? "text-gray-500" : "text-secondary"
+          }`}
+        >
+          {quantity.value}
+        </span>
+      </button>
+      <Toast/>
     </>
   );
 }
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -21,6 +21,8 @@ export default defineApp(async (_req, ctx) => {
         {/* Enable View Transitions API */}
         <meta name="view-transition" content="same-origin" />
 
+        <script src="https://esm.sh/react-toastify@9.1.1?&external=react,react-dom&alias=react/jsx-runtime:preact/jsx-runtime&deps=preact@10.19.2&target=es2022"></script>
+
         {/* Tailwind v3 CSS file */}
         <link
           href={asset(`/styles.css?revision=${revision}`)}
